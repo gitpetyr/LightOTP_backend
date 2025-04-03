72afee3a3b0c56edb5cb8c513e1f75c4a02b1f58
gitpetyr <zhongxiaoma2027@gmail.com>
2025-04-03 20:55:47 +0800

    Upgrade202504032055

--- usermange.py
+++ usermange.py
@@ -6,7 +6,7 @@ import time
 class UserCheck:
     @staticmethod
     def add_userRecord(userid: str, usertoken: str):
-        time.sleep(0.6)  # 添加延迟
+        # time.sleep(0.6)  # 添加延迟
         config.userdbConfig.initDb()  # 初始化数据库
         if not (config.strcheck(userid) and config.strcheck(usertoken)):
             return {
@@ -22,7 +22,7 @@ class UserCheck:
 
     @staticmethod
     def checkUserToken(userid: str, usertoken: str):
-        time.sleep(0.6)  # 添加延迟
+        # time.sleep(0.6)  # 添加延迟
         config.userdbConfig.initDb()  # 初始化数据库
         data = config.userdbConfig.getDb()
         if userid not in data["userinfo"]:
@@ -33,7 +33,7 @@ class UserCheck:
 
     @staticmethod
     def add_totp(userid: str, usertoken: str, totpname: str, totpkey: str):
-        time.sleep(0.6)  # 添加延迟
+        # time.sleep(0.6)  # 添加延迟
         config.userdbConfig.initDb()  # 初始化数据库
         if not (config.strcheck(userid) and config.strcheck(usertoken) and config.strcheck(totpname) and config.strcheck(totpkey)):
             return {
@@ -60,7 +60,7 @@ class UserCheck:
 
     @staticmethod
     def get_totp(userid: str, usertoken: str, totpname: str):
-        time.sleep(0.6)  # 添加延迟
+        # time.sleep(0.6)  # 添加延迟
         config.userdbConfig.initDb()  # 初始化数据库
         data = config.userdbConfig.getDb()
         if userid not in data["userinfo"]:
@@ -77,7 +77,7 @@ class UserCheck:
 
     @staticmethod
     def get_totplist(userid: str, usertoken: str):
-        time.sleep(0.6)  # 添加延迟
+        # time.sleep(0.6)  # 添加延迟
         config.userdbConfig.initDb()  # 初始化数据库
         data = config.userdbConfig.getDb()
         if userid not in data["userinfo"]:
@@ -92,7 +92,7 @@ class UserCheck:
 
     @staticmethod
     def del_totp(userid: str, usertoken: str, totpname: str):
-        time.sleep(0.6)  # 添加延迟
+        # time.sleep(0.6)  # 添加延迟
         config.userdbConfig.initDb()  # 初始化数据库
         data = config.userdbConfig.getDb()
         if userid not in data["userinfo"]:
